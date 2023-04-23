1a7beb66f60312b0143b675bced1ffb201283427
Gaby_Pacheco <dianapachecodiego@gmail.com>
2023-04-23 14:27:15 -0600

    Agregar archivos de proyecto.

---
+++ Administrador/AdminHorarios.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaLab.Administrador
+{
+    public partial class AdminHorarios : Form
+    {
+        public AdminHorarios()
+        {
+            InitializeComponent();
+        }
+    }
+}
